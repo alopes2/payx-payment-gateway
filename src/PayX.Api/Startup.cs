349f0c13700badb042e695d8497c21b083602a96
Andre Lopes <andrelo@dunnhumby.com>
2020-03-20 08:43:41 +0100

    Add Swagger support with Swashbuckle.AspNetCore

--- src/PayX.Api/Startup.cs
+++ src/PayX.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using PayX.Api.Extensions;
 using Serilog;
 
@@ -28,6 +29,11 @@ namespace PayX.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PayX - Payment Gateway", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +57,14 @@ namespace PayX.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PayX V1");
+                c.RoutePrefix = string.Empty;
             });
         }
     }
